140b987aed8445d83519ff22ef9b48c9286af5ae
rungene <rungenesamuel@gmail.com>
2025-07-03 13:03:24 +0300

    Updating model to estate.property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,5 +4,7 @@ from odoo import fields, models
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = 'Estate Propery plans'
+
+    name = fields.Char()
