ee3db3be8b7e2a6d0e1e8ca303a115d69bb2dd5a
rungene <rungenesamuel@gmail.com>
2025-07-04 16:13:32 +0300

    Add basic fields to the table

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,3 +8,18 @@ class EstateProperty(models.Model):
     _description = 'Estate Propery plans'
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Int()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Boolean()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'),
+                   ('east', 'East'), ('west', 'West')])
