f04b29774578ff004d0226916db5f86bc21d4f51
rungene <rungenesamuel@gmail.com>
2025-08-03 10:27:25 +0300

    Updating the model.Price is read-only. Number of bedrooms and availabilty have default values(3 months).Selling price and availabilty will not be copied when record is duplicated.Reserved fields active and state are added to the model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+from odoo.tools import date_utils
 
 
 class EstateProperty(models.Model):
@@ -10,16 +11,24 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=date_utils.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Boolean()
+    garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        # string='Type',
         selection=[('north', 'North'), ('south', 'South'),
                    ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'), ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')], default='new', copy=False
+    )
