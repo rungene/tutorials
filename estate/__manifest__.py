e7667a59ce3f23af580380765ac2598419816e25
rungene <rungenesamuel@gmail.com>
2025-08-03 10:17:49 +0300

    Defining the menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'estate',
     'data': [
         'security/ir.model.access.csv',
-
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_property_menus.xml'
     ],
 }
