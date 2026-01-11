baff9ee5a174f3f1a58430492cad093ba7253cce
rungene <rungenesamuel@gmail.com>
2026-01-11 18:23:39 +0300

    Update Manifest file with base module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'estate',
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_menus.xml'
+    ],
+    'depends': [
+        'base'
     ],
 }
