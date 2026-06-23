8ee23c707e22e5baf3b90dad76021a8cfe961060
rungene <rungenesamuel@gmail.com>
2026-06-23 18:05:43 +0300

    Updating manifest file, loading security file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'estate',
-    'version': '1.0'
-    'summary':'The Real Estate Advertisment module'
-    'description': '',
-    'website': 'https://www.topplinekenya.com'
+    'version': '1.0',
+    'summary':'The Real Estate Advertisment module',
+    'description': 'This is Real Estate Adversitment module description',
+    'website': 'https://www.topplinekenya.com',
     'data': [
+        'security/estate_security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_menus.xml'
