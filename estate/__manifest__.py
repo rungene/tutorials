a8aba1154137729f4468888e1f79d1c3766a5cfc
rungene <rungenesamuel@gmail.com>
2026-08-13 19:35:28 +0300

    Updating Manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,8 @@
         'security/estate_security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_property_menus.xml'
+        'views/estate_property_menus.xml',
+        'views/estate_property_type_views.xml',
     ],
     'depends': [
         'base'
